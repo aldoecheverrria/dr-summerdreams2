8aec1ad797ddaa3d88ea364c2da537728a1371c7
aldoecheverria <aldoecheverria@yahoo.com.mx>
2021-06-04 22:12:30 -0500

    first commit second attempt

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import MenuIcon from '@material-ui/icons/Menu';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container__left">
+
+      </div>
+      <div className="container__right">
+        <MenuIcon/>
+        <div className="bottom">
+
+        </div>
+      </div>
     </div>
   );
 }
