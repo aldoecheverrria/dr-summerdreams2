0eb85de456a0ce9f2c9abd43ba978bb7d526e56b
aldoecheverria <aldoecheverria@yahoo.com.mx>
2021-06-05 19:30:48 -0500

    working on header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import './App.css';
 import MenuIcon from '@material-ui/icons/Menu';
+import LocalMallIcon from '@material-ui/icons/LocalMall';
+import Header from './Header';
+
 
 function App() {
   return (
     <div className="app">
       <div className="container__left">
-
+        <Header/>
       </div>
       <div className="container__right">
         <MenuIcon/>
         <div className="bottom">
+          <div className="product" >
+            <div className="product__container">
+              <h2>Chinesse outfit in yellow with embroidery</h2>
+            <LocalMallIcon/> 
+            </div>
+            <p>$120.90</p>
+          </div>
+          
 
         </div>
       </div>
---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import InstagramIcon from "@material-ui/icons/Instagram";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import YouTubeIcon from "@material-ui/icons/YouTube";
+
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="icons">
+            <InstagramIcon/>
+            <TwitterIcon/>
+            <FacebookIcon/>
+            <YouTubeIcon/>
+          </div>
+        </div>
+    )
+}
+
+export default Header
